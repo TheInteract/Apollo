c316b74cdd1b73d31db265a013d332f1e3f8fbc9
pNagi <nagisama.j@gmail.com>
2017-04-15 12:21:06 +0700

    remove NodeType

--- src/server/graphql/types/index.js
+++ src/server/graphql/types/index.js
@@ -1,13 +1,11 @@
 import FeatureType from './FeatureType'
 import InputProportionType from './InputProportionType'
-import NodeType from './NodeType'
 import ProductType from './ProductType'
 import SessionType from './SessionType'
 
 export {
   FeatureType,
   InputProportionType,
-  NodeType,
   ProductType,
   SessionType,
 }
