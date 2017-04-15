5aea672e8f36de32a4a866f661240ef47d46411d
ChinCluBi <chinnaporn.s@ku.th>
2017-04-16 01:29:46 +0700

    :hammer: remove unused router component

--- src/shared/nt-core/App.react.js
+++ src/shared/nt-core/App.react.js
@@ -1,4 +1,3 @@
-import PropTypes from 'prop-types'
 import React from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
 
@@ -6,26 +5,11 @@ import ProductListPage from '../nt-products/ProductListPage.react'
 import MainPage from './MainPage.react'
 import NoMatch from './NoMatch.react'
 
-const MainRouter = ({ component: Component, computedMatch, ...rest }) => (
-  <Route {...Object.assign(computedMatch, rest)} render={props =>
-    (computedMatch.params.productId) ? (
-      <Component {...props} />
-    ) : (
-      <Redirect to={{ pathname: '/products' }} />
-    )
-  } />
-)
-
-MainRouter.propTypes = {
-  component: PropTypes.func.isRequired,
-  computedMatch: PropTypes.object
-}
-
 const App = () => (
   <Switch>
     <Redirect exact path='/' to={{ pathname: '/products' }} />
     <Route exact path='/products' component={ProductListPage} />
-    <MainRouter path='/products/:productId' component={MainPage} />
+    <Route path='/products/:productId' component={MainPage} />
     <Route component={NoMatch} />
   </Switch>
 )
