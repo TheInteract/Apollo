0df62c87ea88b65fd130ed114e826aa13b126e77
pNagi <nagisama.j@gmail.com>
2017-04-18 13:29:25 +0700

    :hammer: refactor object id validation in query root type

--- src/server/graphql/QueryRootType.js
+++ src/server/graphql/QueryRootType.js
@@ -8,18 +8,20 @@ import Mongodb from 'mongodb'
 import * as Collections from '../mongodb/Collections'
 import { FeatureType, ProductType, SessionType } from './types'
 
+export const validateId = function (id) {
+  return id.match(/^[0-9a-fA-F]{24}$/)
+}
+
 const QueryRootType = new GraphQLObjectType({
   name: 'Query',
   fields: () => ({
     product: {
       type: ProductType,
       args: { _id: { type: GraphQLString } },
-      resolve: async (_, { _id }) => {
-        return _id.match(/^[0-9a-fA-F]{24}$/)
-          ? Collections.findOne('product', {
-            _id: Mongodb.ObjectId(_id)
-          }) : null
-      }
+      resolve: async (_, { _id }) => validateId(_id)
+        ? Collections.findOne('product', {
+          _id: Mongodb.ObjectId(_id)
+        }) : null
     },
     products: {
       type: new GraphQLList(ProductType),
@@ -30,34 +32,26 @@ const QueryRootType = new GraphQLObjectType({
     feature: {
       type: FeatureType,
       args: { _id: { type: GraphQLString } },
-      resolve: async (_, { _id }) => {
-        return _id.match(/^[0-9a-fA-F]{24}$/)
-          ? Collections.findOne('feature', {
-            _id: Mongodb.ObjectId(_id)
-          }) : null
-      }
+      resolve: async (_, { _id }) => validateId(_id)
+        ? Collections.findOne('feature', {
+          _id: Mongodb.ObjectId(_id)
+        }) : null
     },
     features: {
       type: new GraphQLList(FeatureType),
       args: { productId: { type: GraphQLString } },
-      resolve: async (_, { productId }) => {
-        return productId.match(/^[0-9a-fA-F]{24}$/)
-          ? Collections.find('feature', {
-            productId: Mongodb.ObjectId(productId)
-          }, { _id: -1 }) : []
-      }
+      resolve: async (_, { productId }) => validateId(productId)
+        ? Collections.find('feature', {
+          productId: Mongodb.ObjectId(productId)
+        }, { _id: -1 }) : []
     },
     sessions: {
       type: new GraphQLList(SessionType),
       args: { sessionTypeId: { type: GraphQLString } },
-      resolve: async (_, { sessionTypeId }) => {
-        const sessions = await sessionTypeId.match(/^[0-9a-fA-F]{24}$/)
-          ? Collections.find('session', {
-            sessionTypeId: Mongodb.ObjectId(sessionTypeId)
-          }) : []
-
-        return sessions
-      }
+      resolve: async (_, { sessionTypeId }) => validateId(sessionTypeId)
+        ? Collections.find('session', {
+          sessionTypeId: Mongodb.ObjectId(sessionTypeId)
+        }) : []
     },
   })
 })
