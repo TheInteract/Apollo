1ca6cef21437000ebd134b04998c248cc14a55cb
ChinCluBi <chinnaporn.s@ku.th>
2017-04-26 05:39:03 +0700

    :sparkles: added font loader, install Luna font, and add Interact Today title

--- src/shared/nt-home/HomePage.react.js
+++ src/shared/nt-home/HomePage.react.js
@@ -1,5 +1,15 @@
+import className from 'classnames'
 import React from 'react'
 
+import styles from './HomePage.styl'
+
 export default () => (
-  <div>Home</div>
+  <div className={styles.nt}>
+    <div className={className(styles.nt__title, styles.nt__right)}>
+      Interact
+      <div className={styles.nt__subtitle}>
+        today
+      </div>
+    </div>
+  </div>
 )
